f7f8f407a2a573bdfc525f0353ea445b67d7d9b2
noraahlman23 <819446@mystma.org>
2021-01-12 16:31:23 +0000

    diff21

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def diff21(n):
+    if abs (n-21) < 21:
+        return abs (n-21)
+    elif n > 21:
+        return (abs (n-21)) * 2
+    else:
+        return 0
+
+print (diff21(25))
